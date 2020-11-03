9866ae34334ef6ee55b055548b04a653e2d0ba69
peaky76 <robertjamespeacock@gmail.com>
2020-11-03 15:55:45 +0000

    add js card array

---
+++ app.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //card options
+  const cardArray = [
+    {
+      name: "apple",
+      img: "images/png/apple.png",
+    },
+    {
+      name: "orange",
+      img: "images/png/orange.png",
+    },
+    {
+      name: "pear",
+      img: "images/png/pear.png",
+    },
+    {
+      name: "grapes",
+      img: "images/png/grapes.png",
+    },
+    {
+      name: "banana",
+      img: "images/png/banana.png",
+    },
+    {
+      name: "cherries",
+      img: "images/png/cherries.png",
+    },
+    {
+      name: "raspberry",
+      img: "images/png/raspberry.png",
+    },
+    {
+      name: "pineapple",
+      img: "images/png/pineapple.png",
+    },
+    {
+      name: "strawberry",
+      img: "images/png/strawberry.png",
+    },
+    {
+      name: "lemon",
+      img: "images/png/lemon.png",
+    },
+    {
+      name: "apple",
+      img: "images/png/apple.png",
+    },
+    {
+      name: "orange",
+      img: "images/png/orange.png",
+    },
+    {
+      name: "pear",
+      img: "images/png/pear.png",
+    },
+    {
+      name: "grapes",
+      img: "images/png/grapes.png",
+    },
+    {
+      name: "banana",
+      img: "images/png/banana.png",
+    },
+    {
+      name: "cherries",
+      img: "images/png/cherries.png",
+    },
+    {
+      name: "raspberry",
+      img: "images/png/raspberry.png",
+    },
+    {
+      name: "pineapple",
+      img: "images/png/pineapple.png",
+    },
+    {
+      name: "strawberry",
+      img: "images/png/strawberry.png",
+    },
+    {
+      name: "lemon",
+      img: "images/png/lemon.png",
+    },
+  ];
+});
