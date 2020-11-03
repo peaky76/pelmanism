24a3f841b3bb2b1a118b2843a9a133c5095d1b23
peaky76 <robertjamespeacock@gmail.com>
2020-11-03 16:16:38 +0000

    card randomisation added

--- app.js
+++ app.js
@@ -83,9 +83,13 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   ];
 
+  cardArray.sort(() => 0.5 - Math.random());
+
   const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
   var cardsChosen = [];
   var cardsChosenId = [];
+  var cardsWon = [];
 
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
@@ -97,11 +101,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  //check for matches
+  function checkForMatch() {
+    var cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+    if (cardChosen[0] === cardsChosen[1]) {
+      alert("You found a match");
+      cards[optionOneId].setAttribute("src", "images/blank.jpg");
+      cards[optionTwoId].setAttribute("src", "images/blank.jpg");
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionOneId].setAttribute("src", "images/card_back.png");
+      cards[optionTwoId].setAttribute("src", "images/card_back.png");
+      alert("Sorry, try again");
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length == cardArray.length / 2) {
+      resultDisplay.textContent = "Congratulations! You matched them all!";
+    }
+  }
 
   function flipCard() {
     var cardId = this.getAttribute("data-id");
     cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
   }
 
   createBoard();
